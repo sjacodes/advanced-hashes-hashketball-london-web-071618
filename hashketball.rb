b7af7ed97040235e4869c346508103e8748762f6
Sarah Jacob <smjacob91@gmail.com>
2018-07-11 19:17:11 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,23 +200,24 @@ end
 
 
 
+
 def big_shoe_rebounds
-  shoe_size = {}
-  ref_size = 0
-  game_hash.each do |team, team_data| 
-    team_data.each do |key, value| 
-      if key == :players
-        values.each do |name, stats| 
-        stats.select do |stat_name, stat_val| 
+shoe_size = {}
+ref_size = 0
+  game_hash.each { |team, team_attr| 
+    team_attr.each { |attr, values| 
+      if attr == :players
+        values.each { |name, stats| 
+        stats.select {|stat_name, stat_val| 
           if stat_name == :shoe && stat_val > ref_size
-              shoe_size[stat_name] = stat_val
-              shoe_size[:rebounds] = game_hash[team][:players][name][:rebounds]
+              shoe_size[stat_name]=stat_val
+              shoe_size[:rebounds]= game_hash[team][:players][name][:rebounds]
               ref_size = stat_val
-            end
           end
-        end
-      end
-    end
-  end 
-  return shoe_size[:rebounds]
+          }
+          }
+       end
+      }
+    } 
+shoe_size[:rebounds]
 end
